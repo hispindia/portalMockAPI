399c9e7c4c331c10793ddfcf568750e37fc8bd20
Vicky Rohilla <rohillavicky.172@gmail.com>
2018-08-16 15:52:25 +0530

    Password Removal
    Password Removal

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -4,8 +4,8 @@
 exports.DHIS_URL_BASE = "https://uphmis.in/uphmis/";
 
 
-exports.username = "dashboard";
-exports.password = "Dashboard@123";
+exports.username = "";
+exports.password = "";
 exports.auth = "Basic " + new Buffer(exports.username + ":" + exports.password).toString("base64");
 
 
