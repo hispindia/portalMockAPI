83df5649229d9a5ae800b26fd36d109c4c57ad0d
Sourabh Bhardwaj <bsourabh191@gmail.com>
2020-11-26 17:41:36 +0530

    update constants

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -4,8 +4,8 @@
 exports.DHIS_URL_BASE = "https://uphmis.in/uphmis/";
 
 
-exports.username = "dashboard";
-exports.password = "!Dashboard@54321";
+exports.username = "";
+exports.password = "";
 exports.auth = "Basic " + new Buffer(exports.username + ":" + exports.password).toString("base64");
 
 
